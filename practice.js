6be2c8d733cdbe95239f329be5543e180ed9330f
bocacode <bocacode@bocacodes-MacBook-Air.local>
2021-07-01 15:10:27 -0400

    first commit

---
+++ practice.js
@@ -0,0 +1,46 @@
+// for(i = 1; i < 100; i+=2){
+// console.log(i)
+// }
+
+
+
+
+
+// Create a function that will convert from Celsius to Fahrenheit 
+
+// function convertCelsiusToFarenheit(celsius){
+// let Fahrenheit = (celsius *(9/5)) + 32
+// console.log(Fahrenheit)
+// }
+
+// let currentTemp = 20
+
+// convertCelsiusToFarenheit(currentTemp)
+
+
+
+// Calculate the sum of numbers in an array of numbers
+
+let array = [21 , 33 , 42 , 51 , 10 , 5]
+let runningTotal = 0
+for(let i = 0; i < array.length; i++){
+  runningTotal += array[i]
+}
+
+
+console.log(runningTotal/array.length)
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
